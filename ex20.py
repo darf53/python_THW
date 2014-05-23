90d560c11167e9981bce21216c74ef4da037c80d
darf53 <jef53@telenet.be>
2014-05-23 23:14:04 +0200

    ex20.py correct the errors

--- ex20.py
+++ ex20.py
@@ -6,7 +6,7 @@ def print_all(f):
   print f.read()
 
 def rewind(f):
-  f.seek()
+  f.seek(0)
 
 def print_a_line(line_count, f):
   print line_count, f.readline()
@@ -24,10 +24,10 @@ rewind(current_file)
 print "Let's print three lines:"
 
 current_line = 1
-print_a_line(current_line, input_file)
+print_a_line(current_line, current_file)
 
 current_line = current_line + 1
-print_a_line(current_line, input_file)
+print_a_line(current_line, current_file)
 
 current_line = current_line + 1
-print_a_line(current_line, input_file)
+print_a_line(current_line, current_file)
