37defa794eef4241f4c322a4a724c7973dcb02e9
darf53 <jef53@telenet.be>
2014-05-27 15:23:31 -0400

    Part2 ex33.py

--- ex33.py
+++ ex33.py
@@ -1,15 +1,22 @@
-i = 0
-numbers = []
 
-while i < 6:
-  print "At the top i is %d" % i
-  numbers.append(i)
+def while_loop(iteration):
+  i = 0
+  numbers = []
+  while i < iteration:
+    print "At the top i is %d" % i
+    numbers.append(i)
 
-  i = i + 1
-  print "Numbers now:", numbers
-  print "At the bottom i is %d" % i
+    i = i + 1
+    print "Numbers now:", numbers
+    print "At the bottom i is %d" % i
+  return numbers
+
+print "Give me the number of iterations:"
+times = int(raw_input("> "))
+
+list_numbers = while_loop(times)
 
 print "The Numbers:"
 
-for num in numbers:
+for num in list_numbers:
   print "\t", num
