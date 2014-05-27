0d39d140ce78ad3159f91e0de5179327c2ad6992
darf53 <jef53@telenet.be>
2014-05-27 15:26:34 -0400

    Part3 ex33.py

--- ex33.py
+++ ex33.py
@@ -1,20 +1,21 @@
 
-def while_loop(iteration):
+def while_loop(iteration,increment):
   i = 0
   numbers = []
   while i < iteration:
     print "At the top i is %d" % i
     numbers.append(i)
 
-    i = i + 1
+    i = i + increment
     print "Numbers now:", numbers
     print "At the bottom i is %d" % i
   return numbers
 
-print "Give me the number of iterations:"
+print "Give me the number of iterations and an increment number:"
 times = int(raw_input("> "))
+plus = int(raw_input("> "))
 
-list_numbers = while_loop(times)
+list_numbers = while_loop(times,plus)
 
 print "The Numbers:"
 
