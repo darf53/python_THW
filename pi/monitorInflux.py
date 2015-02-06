49b97f1c6947111917b2edaa868b74030e4761a3
darf53 <jef53@telenet.be>
2015-02-06 21:29:22 +0000

    updated data to numbers

--- pi/monitorInflux.py
+++ pi/monitorInflux.py
@@ -1,19 +1,17 @@
 import thermometer
 import thermometerOut
-import getLogins
-#import gspread
-import datetime
 import uploadInflux
 
-strTemp = str(round(thermometer.temp("c"),1))
-print "Current temperature is: " + strTemp + " C"
+#strTemp = str(round(thermometer.temp("c"),1))
+#print "Current temperature is: " + strTemp + " C"
 
 tempOutValues = thermometerOut.values()
-strTempOut = str(tempOutValues[0])
-strHumOut = str(tempOutValues[1])
-print "Current temperature outside is: " + strTempOut + " C"
-print "Current relative humisitye outside is: " + strHumOut + "%"
+#strTempOut = str(tempOutValues[0])
+#strHumOut = str(tempOutValues[1])
+#print "Current temperature outside is: " + strTempOut + " C"
+#print "Current relative humisitye outside is: " + strHumOut + "%"
 
 print "adding data to timesdatabase"
-uploadInflux.upload(strTemp, strTempOut, strHumOut)
+#uploadInflux.upload(strTemp, strTempOut, strHumOut)
+uploadInflux.upload(round(thermometer.temp("c"),1), tempOutValues[0], tempOutValues[1])
 
