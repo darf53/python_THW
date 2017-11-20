f07f19996617d300be161d01a85d865c86248cda
darf53 <jef53@telenet.be>
2017-11-20 20:42:53 +0100

    upload to emoncms.org

--- pi/uploadSolar.py
+++ pi/uploadSolar.py
@@ -1,12 +1,17 @@
 import requests
 import getLogins
+from influxdb import InfluxDBClient
 
 def getApiKey():
-  login = getLogins.getLogins()
-  return(login[4])
+    login = getLogins.getLogins()
+    return(login[4])
 
 def uploadSolar(pulse):
-  apikey = getApiKey()
-  url = 'https://emoncms.org/input/post?node=solar&apikey=' + apikey + '&fulljson={"pulse": ' + str(pulse) + '}'
-  r = requests.post(url)
+    apikey = getApiKey()
+    url = 'https://emoncms.org/input/post?node=solar&apikey=' + apikey + '&fulljson={"pulse": ' + str(pulse) + '}'
+    r = requests.post(url)
+
+
+
+
 
